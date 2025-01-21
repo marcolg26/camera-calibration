80aef9a9fdf30aced7d48dcf64362eddf964610e
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2025-01-21 22:24:07 +0100

    Reformatting

--- functions.py
+++ functions.py
@@ -3,22 +3,24 @@ import numpy as np
 import cv2
 import os
 
+
 def estimate_homographies(path, grid_size, square_size):
-    
+
     img = os.path.basename(path).split('.')[0]
     image = cv2.imread(path)
 
-    return_value, corners = cv2.findChessboardCorners(image, patternSize=grid_size)
+    return_value, corners = cv2.findChessboardCorners(
+        image, patternSize=grid_size)
 
     if return_value is False:
         print(f'{img} corners not found')
         return False, 0, 0, 0
 
-    corners=corners.reshape((grid_size[0]*grid_size[1],2)).copy()
+    corners = corners.reshape((grid_size[0]*grid_size[1], 2)).copy()
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 100, 0.001)
-    cv2.cornerSubPix(gray,corners,(5,5),(-1,-1),criteria)
+    cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria)
 
     real_coordinates = np.empty_like(corners)
 
@@ -30,7 +32,7 @@ def estimate_homographies(path, grid_size, square_size):
         x_mm = (u_index) * square_size
         y_mm = (v_index) * square_size
 
-        real_coordinates[index,:] = [x_mm, y_mm]
+        real_coordinates[index, :] = [x_mm, y_mm]
 
     A = np.empty((0, 9), dtype=float)
 
@@ -48,27 +50,28 @@ def estimate_homographies(path, grid_size, square_size):
 
     U, S, Vtransposed = np.linalg.svd(A)
 
-    h=Vtransposed.transpose()[:,-1]
-    H = h.reshape(3, 3) 
+    h = Vtransposed.transpose()[:, -1]
+    H = h.reshape(3, 3)
 
     return True, H, corners, real_coordinates
+
 
 def compute_K(HH):
 
     V = np.empty((0, 6), dtype=float)
 
     for HI in HH:
-        V = np.vstack((V, np.hstack(vij_matrix(HI,0,1))))
-        V = np.vstack((V, np.hstack(vij_matrix(HI,0,0)- vij_matrix(HI,1,1))))
-
+        V = np.vstack((V, np.hstack(vij_matrix(HI, 0, 1))))
+        V = np.vstack(
+            (V, np.hstack(vij_matrix(HI, 0, 0) - vij_matrix(HI, 1, 1))))
 
     U, sigma, Stransposed = np.linalg.svd(V)
 
-    b = Stransposed.transpose()[:,-1]
+    b = Stransposed.transpose()[:, -1]
 
-    B=[[b[0], b[1], b[3]],
-       [b[1], b[2], b[4]],
-       [b[3], b[4], b[5]]]
+    B = [[b[0], b[1], b[3]],
+         [b[1], b[2], b[4]],
+         [b[3], b[4], b[5]]]
 
     B = np.array(B)
 
@@ -82,34 +85,36 @@ def compute_K(HH):
         print("error in the calculation of Cholesky factorization")
 
     K = np.linalg.inv(L.transpose())
-    K = K/K[2,2]
+    K = K/K[2, 2]
 
     return K
 
+
 def vij_matrix(H, i, j):
 
-    vij = np.array([H[0,i]*H[0,j],
-                    H[0,i]*H[1,j] + H[1,i]*H[0,j],
-                    H[1,i]*H[1,j],
-                    H[2,i]*H[0,j] + H[0,i]*H[2,j],
-                    H[2,i]*H[1,j] + H[1,i]*H[2,j],
-                    H[2,i]*H[2,j]])
-    
+    vij = np.array([H[0, i]*H[0, j],
+                    H[0, i]*H[1, j] + H[1, i]*H[0, j],
+                    H[1, i]*H[1, j],
+                    H[2, i]*H[0, j] + H[0, i]*H[2, j],
+                    H[2, i]*H[1, j] + H[1, i]*H[2, j],
+                    H[2, i]*H[2, j]])
+
     vij = vij.transpose()
 
     return vij
 
+
 def compute_Rt(H, K):
 
-    lambd = 1/(np.linalg.norm(np.linalg.inv(K)@ H[:,0]))
+    lambd = 1/(np.linalg.norm(np.linalg.inv(K) @ H[:, 0]))
 
-    r1 = lambd*np.linalg.inv(K)@H[:,0]
-    r2 = lambd*np.linalg.inv(K)@H[:,1]
+    r1 = lambd*np.linalg.inv(K)@H[:, 0]
+    r2 = lambd*np.linalg.inv(K)@H[:, 1]
     r3 = np.cross(r1, r2)
     R = np.column_stack((r1, r2, r3))
-    
-    t = lambd*np.linalg.inv(K)@H[:,2]
-    t= t.reshape(3, 1)
+
+    t = lambd*np.linalg.inv(K)@H[:, 2]
+    t = t.reshape(3, 1)
 
     if not np.allclose(R.transpose(), np.linalg.inv(R)):
         U, S, Vtransposed = np.linalg.svd(R)
@@ -117,6 +122,7 @@ def compute_Rt(H, K):
         R = Rprime
 
     return R, t
+
 
 def compute_error(image):
 
@@ -128,11 +134,13 @@ def compute_error(image):
     for i, point in enumerate(corners):
 
         m = np.array([real_coordinates[i, 0], real_coordinates[i, 1], 0, 1])
-        error = error + ((P[0, :]@m)/(P[2, :]@m) - point[0])**2 + ((P[1, :]@m)/(P[2, :]@m) - point[1])**2
+        error = error + ((P[0, :]@m)/(P[2, :]@m) - point[0]
+                         )**2 + ((P[1, :]@m)/(P[2, :]@m) - point[1])**2
 
-    error=round(error, 2)
+    error = round(error, 2)
     image["error"] = error
     return error
+
 
 def draw_points(figure, image):
 
@@ -143,10 +151,12 @@ def draw_points(figure, image):
     for i, point in enumerate(corners):
 
         m = np.array([real_coordinates[i, 0], real_coordinates[i, 1], 0, 1])
-        cv2.circle(figure, (round(point[0]),round(point[1])), 5, color=(0,0,255), thickness=1)
-        cv2.circle(figure, (round((P[0, :]@m)/(P[2, :]@m) ),round((P[1, :]@m)/(P[2, :]@m))), 5, color=(255,0,0), thickness=1)
-    
-    plt.figure(num = f'{image["name"]}', figsize = (6.4*2,4.8*2))
+        cv2.circle(figure, (round(point[0]), round(
+            point[1])), 5, color=(0, 0, 255), thickness=1)
+        cv2.circle(figure, (round((P[0, :]@m)/(P[2, :]@m)), round(
+            (P[1, :]@m)/(P[2, :]@m))), 5, color=(255, 0, 0), thickness=1)
+
+    plt.figure(num=f'{image["name"]}', figsize=(6.4*2, 4.8*2))
     plt.axis('off')
     plt.imshow(figure)
 
@@ -154,7 +164,7 @@ def draw_points(figure, image):
     text += f't={np.round(image["t"].transpose(),3)}'
     text += '$^{T}$\n'
     text += f'error={image["error"]}'
-    
+
     t = plt.text(.01, .99, text, ha='left', va='top', color='black')
     t.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='white'))
 
@@ -165,8 +175,10 @@ def draw_points(figure, image):
 
     if not os.path.exists(path):
         os.makedirs(path)
-    plt.savefig(f'{path}/{image["name"]} - points.png', bbox_inches='tight', pad_inches = 0)
+    plt.savefig(f'{path}/{image["name"]} - points.png',
+                bbox_inches='tight', pad_inches=0)
     plt.show()
+
 
 def superimpose_cylinder(figure, image, a, b, r, h):
 
@@ -185,45 +197,54 @@ def superimpose_cylinder(figure, image, a, b, r, h):
         positionsY[i] = r * np.sin(t) + b
         positionsZ[i] = 0
         t += stepSize
-        
-    circle_inf = P @ np.vstack((positionsX, positionsY, positionsZ, np.ones_like((positionsX))))
-    if tz >= 0:
-        circle_sup = P @ np.vstack((positionsX, positionsY, positionsZ + h, np.ones_like((positionsX))))
-    else:
-        circle_sup = P @ np.vstack((positionsX, positionsY, positionsZ - h, np.ones_like((positionsX))))
 
+    circle_inf = P @ np.vstack((positionsX, positionsY,
+                               positionsZ, np.ones_like((positionsX))))
+    if tz >= 0:
+        circle_sup = P @ np.vstack((positionsX, positionsY,
+                                   positionsZ + h, np.ones_like((positionsX))))
+    else:
+        circle_sup = P @ np.vstack((positionsX, positionsY,
+                                   positionsZ - h, np.ones_like((positionsX))))
 
     circle_inf = (circle_inf/circle_inf[2])[:2]
     circle_sup = (circle_sup/circle_sup[2])[:2]
 
     overlay = figure.copy()
 
-    cv2.polylines(figure, np.int32([circle_inf.transpose()]), isClosed=True, color=(0, 0, 0), thickness=2)
-    cv2.fillPoly(overlay, np.int32([circle_inf.transpose()]), color=(255, 0, 0))
+    cv2.polylines(figure, np.int32(
+        [circle_inf.transpose()]), isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.fillPoly(overlay, np.int32(
+        [circle_inf.transpose()]), color=(255, 0, 0))
 
     alpha = 0.7
     figure = cv2.addWeighted(overlay, alpha, figure, 1 - alpha, 0)
 
     overlay = figure.copy()
 
-    cv2.polylines(figure, np.int32([circle_sup.transpose()]), isClosed=True, color=(0, 0, 0), thickness=2)
-    cv2.fillPoly(overlay, np.int32([circle_sup.transpose()]), color=(0, 255, 0))
+    cv2.polylines(figure, np.int32(
+        [circle_sup.transpose()]), isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.fillPoly(overlay, np.int32(
+        [circle_sup.transpose()]), color=(0, 255, 0))
 
     figure = cv2.addWeighted(overlay, alpha, figure, 1 - alpha, 0)
-    
+
     alpha = 0.2
     for i in range(h):
         overlay = figure.copy()
         if tz >= 0:
-            circle_int = P @ np.vstack((positionsX, positionsY, positionsZ + i, np.ones_like((positionsX))))
+            circle_int = P @ np.vstack((positionsX, positionsY,
+                                       positionsZ + i, np.ones_like((positionsX))))
         else:
-            circle_int = P @ np.vstack((positionsX, positionsY, positionsZ - i, np.ones_like((positionsX))))
+            circle_int = P @ np.vstack((positionsX, positionsY,
+                                       positionsZ - i, np.ones_like((positionsX))))
 
         circle_int = (circle_int/circle_int[2])[:2]
-        cv2.polylines(overlay, np.int32([circle_int.transpose()]), isClosed=True, color=(128, 128, 128), thickness=1)
+        cv2.polylines(overlay, np.int32(
+            [circle_int.transpose()]), isClosed=True, color=(128, 128, 128), thickness=1)
         figure = cv2.addWeighted(overlay, alpha, figure, 1 - alpha, 0)
 
-    plt.figure(num = f'{image["name"]} - cylinder', figsize = (6.4*2,4.8*2))
+    plt.figure(num=f'{image["name"]} - cylinder', figsize=(6.4*2, 4.8*2))
     plt.axis('off')
     plt.imshow(figure)
 
@@ -234,8 +255,8 @@ def superimpose_cylinder(figure, image, a, b, r, h):
 
     if not os.path.exists(path):
         os.makedirs(path)
-    plt.savefig(f'{path}/{image["name"]} - cylinder.png', bbox_inches='tight', pad_inches = 0)
+    plt.savefig(f'{path}/{image["name"]} - cylinder.png',
+                bbox_inches='tight', pad_inches=0)
     plt.show()
 
     return 0
-
--- main.py
+++ main.py
@@ -6,27 +6,31 @@ import cv2
 from mpmath import *
 from functions import *
 
+
 def calibrate_camera(personal=0, grid_size_x=8, grid_size_y=11, square_size=11):
 
     if personal == 0:
         folderpath = os.path.dirname(os.path.abspath(__file__)) + "\\images\\"
     else:
-        folderpath = os.path.dirname(os.path.abspath(__file__)) + "\\images\\personal\\"
+        folderpath = os.path.dirname(
+            os.path.abspath(__file__)) + "\\images\\personal\\"
 
     image_data = {}
     K_parameters = {}
 
-    images_path = [os.path.join(folderpath, imagename) for imagename in os.listdir(folderpath) if( imagename.endswith(".tiff") or imagename.endswith(".tif"))]
+    images_path = [os.path.join(folderpath, imagename) for imagename in os.listdir(
+        folderpath) if (imagename.endswith(".tiff") or imagename.endswith(".tif"))]
     images_path.sort()
 
-    Hk=[]
+    Hk = []
     for p in images_path:
 
         img = os.path.basename(p).split('.')[0]
         image_data[img] = {}
 
         grid_size = (grid_size_x, grid_size_y)
-        found, H, corners, real_coordinates = estimate_homographies(p, grid_size, square_size)
+        found, H, corners, real_coordinates = estimate_homographies(
+            p, grid_size, square_size)
 
         image_data[img]["name"] = img
 
@@ -34,16 +38,15 @@ def calibrate_camera(personal=0, grid_size_x=8, grid_size_y=11, square_size=11):
             image_data[img]["personal"] = True
         else:
             image_data[img]["personal"] = False
-        
+
         if found is True:
             Hk.append(H)
             image_data[img]["corners"] = corners
             image_data[img]["real_coordinates"] = real_coordinates
             image_data[img]["H"] = H
-        
+
         else:
             image_data[img]["corners"] = False
-
 
     K = compute_K(Hk)
 
@@ -55,7 +58,7 @@ def calibrate_camera(personal=0, grid_size_x=8, grid_size_y=11, square_size=11):
 
             H = image_data[img]["H"]
 
-            lambd = 1/(np.linalg.norm(np.linalg.inv(K)@H[:,0]))
+            lambd = 1/(np.linalg.norm(np.linalg.inv(K)@H[:, 0]))
             image_data[img]["lambda"] = lambd
 
             R, t = compute_Rt(H, K)
@@ -65,11 +68,11 @@ def calibrate_camera(personal=0, grid_size_x=8, grid_size_y=11, square_size=11):
             P = K @ np.hstack((R, t))
             image_data[img]["P"] = P
 
-    K_parameters["alphau"] = K[0,0]
-    K_parameters["skew"] = acot(K[0,1]/K_parameters["alphau"])
-    K_parameters["alphav"] = K[1,1]/sin(K_parameters["skew"])
-    K_parameters["u0"] = K[0,2]
-    K_parameters["v0"] = K[1,2]
+    K_parameters["alphau"] = K[0, 0]
+    K_parameters["skew"] = acot(K[0, 1]/K_parameters["alphau"])
+    K_parameters["alphav"] = K[1, 1]/sin(K_parameters["skew"])
+    K_parameters["u0"] = K[0, 2]
+    K_parameters["v0"] = K[1, 2]
 
     print(f'K = {K}')
     print(f'alphau = {K_parameters["alphau"]}')
@@ -83,15 +86,15 @@ def calibrate_camera(personal=0, grid_size_x=8, grid_size_y=11, square_size=11):
         img = os.path.basename(p).split('.')[0]
 
         image = cv2.imread(p)
-        image2=image.copy()
+        image2 = image.copy()
 
         if image_data[img]["corners"] is not False:
 
             compute_error(image_data[img])
 
             draw_points(image, image_data[img])
-            xx=round(max(image_data[img]["real_coordinates"][:, 0])/2)
-            yy=round(max(image_data[img]["real_coordinates"][:, 1])/2)
+            xx = round(max(image_data[img]["real_coordinates"][:, 0])/2)
+            yy = round(max(image_data[img]["real_coordinates"][:, 1])/2)
 
             superimpose_cylinder(image2, image_data[img], xx, yy, 30, 75)
 
@@ -103,7 +106,7 @@ if __name__ == "__main__":
         grid_size_x = int(sys.argv[2])
         grid_size_y = int(sys.argv[3])
         square_size = int(sys.argv[4])
-        
+
         calibrate_camera(personal, grid_size_x, grid_size_y, square_size)
 
     else:
